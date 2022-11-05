1e3b338f396591e60a839b297ebb1049f2cc7685
LukeLook <lukeluocn@outlook.com>
2022-11-05 08:18:42 +0000

    fix: run deferred callbacks

--- main.go
+++ main.go
@@ -6,9 +6,11 @@ import (
 	"github.com/hashicorp/terraform-plugin-framework/providerserver"
 
 	"github.com/smc-x/terraform-provider-smc/smc"
+	"github.com/smc-x/terraform-provider-smc/smc/global"
 )
 
 func main() {
+	defer global.Run()
 	providerserver.Serve(context.Background(), smc.New, providerserver.ServeOpts{
 		Address: "registry.terraform.io/smc-x/smc",
 	})
