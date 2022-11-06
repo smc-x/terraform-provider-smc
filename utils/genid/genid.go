f3c8e47d2c4c4241bf76326ee0a71abdbb2dda3b
LukeLook <lukeluocn@outlook.com>
2022-11-06 08:36:05 +0000

    fix: do not lower the id

--- utils/genid/genid.go
+++ utils/genid/genid.go
@@ -2,11 +2,9 @@
 package genid
 
 import (
-	"strings"
-
 	"github.com/teris-io/shortid"
 )
 
 func Short() string {
-	return strings.ToLower(shortid.MustGenerate())
+	return shortid.MustGenerate()
 }
